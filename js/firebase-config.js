03f46bbc5a41693d3d12278054ad86281e9cee44
GSJhan <jhanthupapa@gmail.com>
2026-06-23 11:13:21 -0500

    Delete js directory

--- js/firebase-config.js
+++
@@ -1,8 +0,0 @@
-const firebaseConfig = {
-  apiKey: "AIzaSyBJ2Rzx4w_7UwDl_0iV7uGoe22WIjBCiPg",
-  authDomain: "prep0l-reset.firebaseapp.com",
-  projectId: "prep0l-reset",
-  storageBucket: "prep0l-reset.firebasestorage.app",
-  messagingSenderId: "828820833195",
-  appId: "1:828820833195:web:87eae0c3a0f778eb2908ee"
-};
